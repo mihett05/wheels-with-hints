237af50d9b3c478072cf4c11cd5f7f06d74be1c8
mihett05 <mihett05@yandex.ru>
2022-06-22 20:32:53 +0500

    Added android

---
+++ android/app/src/main/java/com/wheels_with_hints/MainActivity.java
@@ -0,0 +1,28 @@
+package com.wheels_with_hints;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView browser = findViewById(R.id.webBrowser);
+        browser.setWebViewClient(new WebViewClient());
+        WebSettings settings = browser.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        browser.loadUrl("http://94.50.162.171/");
+    }
+}
